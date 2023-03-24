322e01907e63ca9dd2a73a69fd2785fdfb082621
MaximEdogawa <leo@librem.one>
2023-03-25 00:17:45 +0100

    added text and images to overview

---
+++ src/components/NavCard.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+  >
+    <img class="rounded-t-lg" :src="imagePath" alt="" />
+    <div class="p-4">
+      <a :href="linkPath">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {{ headline }}
+        </h5></a
+      >
+
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {{ text }}
+      </p>
+      <a
+        :href="linkPath"
+        class="inline-flex items-center px-3 py-2 text-sm font-bold text-center text-white bg-emerald-500 rounded-lg hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+      >
+        {{ linkText }}
+        <svg
+          aria-hidden="true"
+          class="w-4 h-4 ml-2 -mr-1"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </a>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { link } from "fs";
+import { ref, onMounted } from "vue";
+import raspberry_white from "../assets/raspberry_case_white.png";
+import logo_sign_black from "../assets/logo_sign_black.png";
+import raspberry_gold from "../assets/raspberry_case_gold.png";
+
+const imageNumber = ref(0);
+const imagePath = ref("");
+const headline = ref("");
+const text = ref("");
+const linkText = ref("");
+const linkPath = ref("");
+const props = defineProps({
+  imageNumber: {
+    type: Number,
+    default: 1,
+  },
+  headline: {
+    type: String,
+    default: "",
+  },
+  text: {
+    type: String,
+    default: "",
+  },
+  linkText: {
+    type: String,
+    default: "",
+  },
+  linkPath: {
+    type: String,
+    default: "",
+  },
+});
+
+onMounted(async () => {
+  imageNumber.value = props.imageNumber;
+  headline.value = props.headline;
+  text.value = props.text;
+  linkText.value = props.linkText;
+  linkPath.value = props.linkPath;
+  switch (imageNumber.value) {
+    case 1:
+      imagePath.value = raspberry_white;
+      break;
+    case 2:
+      imagePath.value = logo_sign_black;
+      break;
+    case 3:
+      imagePath.value = raspberry_gold;
+      break;
+    default:
+      imagePath.value = raspberry_white;
+      break;
+  }
+});
+</script>
--- src/views/OverView.vue
+++ src/views/OverView.vue
@@ -1,13 +1,11 @@
 <template>
-    <div
-    class=" bg-fixed justify-center border-2 border-transparent rounded-xl"
+  <div
+    class="bg-fixed justify-center border-2 border-transparent rounded-xl"
     :style="{ backgroundImage: `url(${backgroundUrl})` }"
   >
-    <h1 class=" font-sans font-semibold 2xl:text-5xl sm:text-xl md:text-3xl text-neutral-50 text-center top-4 font- bg-emerald-600 bg-opacity-10 drop-shadow-2xl">
-      Next Level <br>Custody Solution on Chia Network
-    </h1>
-  
-    <div class="flex w-full h-full justify-center p-4 bg-emerald-600 bg-opacity-10">
+    <div
+      class="flex w-full h-full justify-center p-4 bg-emerald-600 bg-opacity-10"
+    >
       <div
         class="block max-w-3xl xs:max-w-l border-4 border-transparent rounded-xl shadow drop-shadow-2xl"
       >
@@ -17,11 +15,190 @@
           alt="Display"
         />
       </div>
-    </div>    
-</div>
-
-  
+    </div>
+    <div
+      class="grid justify-center bg-emerald-600 bg-opacity-10 drop-shadow-2xl"
+    >
+      <h1
+        class="font-sans font-semibold 2xl:text-5xl sm:text-xl md:text-3xl text-neutral-50 text-center"
+      >
+        Next Level <br />Custody Solution on Chia Network
+      </h1>
+      <div
+        class="p-4 flex flex-row justify-items-stretch justify-evenly gap-2 drop-shadow-2xl"
+      >
+        <button
+          class="p-2 font-sans font-bold text-xs 2xl:text-xl sm:text-xs md:text-xs text-neutral-50 border-2 border-emerald-500 shadow-lg shadow-emerald-500/50 rounded-full hover:bg-emerald-500 drop-shadow-2xl"
+        >
+          <a
+            href="https://github.com/MaximEdogawa/seedener/releases"
+            target="_blank"
+          >
+            DOWNLOAD THE LAST RELEASE
+          </a>
+        </button>
+        <button
+          class="p-2 font-sans font-bold text-xs 2xl:text-xl sm:text-xs md:text-xs text-neutral-50 border-2 border-emerald-500 shadow-lg shadow-emerald-500/50 rounded-full hover:bg-emerald-500 drop-shadow-2xl"
+        >
+          <a
+            href="https://github.com/MaximEdogawa/seedener/blob/main/seedsaver_pubkey.gpg"
+            target="_blank"
+          >
+            GPG PUB KEY
+          </a>
+        </button>
+        <button
+          class="p-2 font-sans font-bold text-xs 2xl:text-xl sm:text-xs md:text-xs text-neutral-50 border-2 border-emerald-500 shadow-lg shadow-emerald-500/50 rounded-full hover:bg-emerald-500 drop-shadow-2xl"
+        >
+          <a href="https://github.com/MaximEdogawa/seedener" target="_blank">
+            VIEW THE PROJECT ON GITHUB
+          </a>
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="grid justify-center bg-white">
+    <h1
+      class="p-4 font-sans font-medium 2xl:text-4xl sm:text-xl md:text-3xl text-black-50 text-center drop-shadow-2xl"
+    >
+      BUILD YOUR OWN<br />CHIA SIGNING DEVICE
+    </h1>
+    <p
+      class="p-4 font-sans font-semibold text-base text-black-50 text-center drop-shadow-2xl"
+    >
+      USING A RASPBERRY PI ZERO
+    </p>
+    <p
+      class="font-sans font-medium 2xl:text-xl sm:text-base md:text-3xl text-gray-700 text-center drop-shadow-2xl"
+    >
+      You can build your own Hardware Wallet for Chia blockchain custody
+      solution.
+    </p>
+    <p
+      class="font-sans font-medium 2xl:text-xl sm:text-base md:text-3xl text-gray-700 text-center drop-shadow-2xl"
+    >
+      To generate keys and air-gapped sign spend bundles.
+    </p>
+    <div
+      class="p-4 flex flex-row justify-items-stretch justify-evenly gap-2 drop-shadow-2xl"
+    >
+      <NavCard
+        :image-number="1"
+        headline="Quick Installation"
+        text="The fastest and easiest way to run the latest version of SeedSaver - Sign is to download our most recent release."
+        link-text="latest release"
+        link-path="https://github.com/MaximEdogawa/seedener/releases"
+      ></NavCard>
+      <NavCard
+        :image-number="2"
+        headline="Full Installation"
+        text="Want to maximize your trustlessness? Check out our manual build instructions to build from the source code."
+        link-text="manual build"
+        link-path="https://github.com/MaximEdogawa/seedener/blob/main/docs/manual_installation.md"
+      ></NavCard>
+      <NavCard
+        :image-number="3"
+        headline="Make Your Own Case"
+        text="The Open Pill enclosure design is open source too! Print your own or mod the case design."
+        link-text="design files"
+        link-path="https://github.com/MaximEdogawa/seedener/tree/main/enclosures/open_pill"
+      ></NavCard>
+    </div>
+  </div>
+  <div class="bg-emerald-50">
+    <div class="py-4 flex justify-center">
+      <h1
+        class="w-3/5 font-sans font-medium 2xl:text-4xl sm:text-xl md:text-3xl text-black-50 text-left drop-shadow-2xl"
+      >
+        About SeedSaver
+      </h1>
+    </div>
+    <div class="flex justify-center">
+      <p
+        class="w-3/5 font-sans font-medium 2xl:text-xl sm:text-base md:text-base text-gray-700 text-justify drop-shadow-2xl"
+      >
+        Seedsaver offers anyone the opportunity to build a verifiably
+        air-gapped, stateless Chia signing device using inexpensive, publicly
+        available hardware components. The goal of Seedsaver is to make Chia
+        custody solution more accessible to users. By providing a user interface
+        where they can easily create, transcribe and export Keys for signing
+        spend bundles which are used for Chia custody solution vaults.
+      </p>
+    </div>
+    <div class="py-4 flex justify-center">
+      <p
+        class="w-3/5 font-sans font-medium 2xl:text-xl sm:text-base md:text-base text-gray-700 text-justify drop-shadow-2xl"
+      >
+        Use Seedsaver-Sign to complete these task:
+      </p>
+    </div>
+    <div class="flex justify-center">
+      <ul
+        class="list-disc w-1/2 font-sans font-medium 2xl:text-xl sm:text-base md:text-base text-gray-700 text-justify drop-shadow-2xl"
+      >
+        <li>
+          Create Keys for custody solution
+        </li>
+        <li>
+          Scan Private Keys, Encrypted Priv Key, SpendBundle
+        </li>
+        <li>
+          Backup Private Keys flow
+        </li>
+        <li>
+          Export Public Keys via QR
+        </li>
+        <li>
+          Export Encrypted Private Keys via QR
+        </li>
+        <li>
+          Export Transcribe Private Keys via mark down QR view
+        </li>
+        <li>
+          Sign Spend Bundle with Private Keys
+        </li>
+        <li>
+          Merge Spend Bundle Signed Parts
+        </li>
+        <li>
+          Export Signed Spend Bundle via QR Transfer Loop
+        </li>
+        <li>
+          Rekey Priv Keys
+        </li>
+      </ul>
+    </div>
+    <div class="py-4 flex justify-center">
+      <p
+        class="w-3/5 font-sans font-medium 2xl:text-xl sm:text-base md:text-base text-gray-700 text-justify drop-shadow-2xl"
+      >
+        The key component in SeedSaver is a specific version of the Raspberry Pi
+        Zero single board computer (version 1.3) that does not include WiFi or
+        Bluetooth functionality; the absence of these modules ensures that your
+        private keys never leave the device. Communication with wallet software
+        is accomplished using a two-way QR code transfer loop exchange process.
+        Information about a proposed transaction is displayed by the wallet
+        software on your computer’s screen, encoded in multiple QR codes, and
+        you scan in the transaction information using SeedSaver’s-Sign
+        built-in camera. Information about the proposed transaction is then
+        displayed on the SeedSaver-Sign screen. Using cryptographic
+        signatures, SeedSaver-Sign then proves that it has the correct private
+        keys to authorize the spend bundles, and QR codes demonstrating that
+        signed spend bundle displayed on SeedSaver’s-Sign screen.
+      </p>
+    </div>
+    <div class="py-4 flex justify-center">
+      <p
+        class="w-3/5 font-sans font-medium 2xl:text-xl sm:text-base md:text-base text-gray-700 text-justify drop-shadow-2xl"
+      >
+        You then use your smartphone's camera with Seedsaver-Store companion app
+        to input the QR codes generated by SeedSaver-Sign to authorize the
+        transaction.
+      </p>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import backgroundUrl from "../assets/raspbeerypi_zero_banner.jpg";
+import NavCard from "../components/NavCard.vue";
 </script>
