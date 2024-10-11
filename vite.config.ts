e42ee55be2a24a436cdb67827e3f9c5155a65441
MaximEdogawa <leo@librem.one>
2024-10-11 22:05:13 +0200

    changed vite proxy settings

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,13 @@ export default defineConfig({
     proxy: {
       "/chia.6e1af92cf5e7628be6b5af510352b548163e49c28684cad17ef19527b75657d0":
         {
-          target: "https://localhost:4161",
-          rewrite: (path) => path.replace(/^\/chia\.[\w]+/, "/"),
+          target:
+            "https://localhost:4161/chia.6e1af92cf5e7628be6b5af510352b548163e49c28684cad17ef19527b75657d0",
+          rewrite: (path) =>
+            path.replace(
+              "/chia.6e1af92cf5e7628be6b5af510352b548163e49c28684cad17ef19527b75657d0",
+              "/"
+            ),
         },
     },
   },
